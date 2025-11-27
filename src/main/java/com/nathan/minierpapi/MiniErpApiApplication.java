020e9ea58e5507dc6c1de4dff33a461c75a292c8
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-27 13:36:28 +0300

    Project structure boiler plate code

---
+++ src/main/java/com/nathan/minierpapi/MiniErpApiApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.minierpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniErpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniErpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathan/minierpapi/MiniErpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.minierpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
