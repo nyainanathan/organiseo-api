69961cfa7ec998db921cc63ca78990be0ffaeeef
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-28 18:38:43 +0300

    Feat: create user dto

---
+++ src/main/java/com/nathan/minierpapi/dto/CreateUser.java
@@ -0,0 +1,15 @@
+package com.nathan.minierpapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CreateUser {
+    private String username;
+    private String fullName;
+    private String role;
+    private String password;
+}
