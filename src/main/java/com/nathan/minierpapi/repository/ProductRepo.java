3b377502b425fa49f9cdd934be760661fb3ee9da
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-11-29 10:10:02 +0300

    Feat(produt): product row mapper for faster treatment of queries

---
+++ src/main/java/com/nathan/minierpapi/repository/ProductRepo.java
@@ -0,0 +1,37 @@
+package com.nathan.minierpapi.repository;
+
+import com.nathan.minierpapi.dto.CreateProduct;
+import com.nathan.minierpapi.model.product.Products;
+import com.nathan.minierpapi.utils.TimeUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+@AllArgsConstructor
+public class ProductRepo {
+
+    private JdbcTemplate jdbcTemplate;
+    private TimeUtils timeUtils;
+
+    private Products mapProduct(ResultSet rs, int rowNum) throws SQLException {
+        return new Products(
+                rs.getString("name"),
+                rs.getString("barcode"),
+                rs.getString("sku"),
+                rs.getString("category"),
+                rs.getString("brand"),
+                rs.getFloat("purchase_price"),
+                rs.getFloat("sell_price"),
+                rs.getString("unit"),
+                rs.getString("id"),
+                timeUtils.convertToInstant(rs.getString("created_at")),
+                timeUtils.convertToInstant(rs.getString("updated_at"))
+        );
+    }
+
+
+}
