9d5180416533fac13218ea361fb6ab0da10fb740
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-12-01 10:18:29 +0300

    Feat(inventory): inventoryRowMapper

---
+++ src/main/java/com/nathan/minierpapi/mapper/InventoryRowMapper.java
@@ -0,0 +1,27 @@
+package com.nathan.minierpapi.mapper;
+
+import com.nathan.minierpapi.model.inventory.InventoryItem;
+import com.nathan.minierpapi.utils.TimeUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class InventoryRowMapper implements RowMapper<InventoryItem> {
+
+    private TimeUtils timeUtils;
+
+    @Override
+    public InventoryItem mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new InventoryItem(
+                rs.getString("product_id"),
+                rs.getInt("quantity"),
+                rs.getInt("reserved"),
+                rs.getString("location"),
+                rs.getFloat("average_cost"),
+                timeUtils.convertToInstant(rs.getString("last_updated"))
+        );
+    }
+}
--- src/main/java/com/nathan/minierpapi/repository/InventoryRepo.java
+++ src/main/java/com/nathan/minierpapi/repository/InventoryRepo.java
@@ -1,8 +1,11 @@
 package com.nathan.minierpapi.repository;
 
+import com.nathan.minierpapi.model.inventory.InventoryItem;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
 
 //TODO: Post route for creating inventory movement
 //TODO: Get routes for fetching all movement and one movement
@@ -13,6 +16,9 @@ public class InventoryRepo {
 
     private JdbcTemplate jdbcTemplate;
 
-
+    private InventoryItem getInventoryItemById(String id) throws SQLException {
+        String selectQuery = "SELECT * FROM inventory_items WHERE id = ?";
+        return jdbcTemplate.queryForObject()
+    }
 
 }
