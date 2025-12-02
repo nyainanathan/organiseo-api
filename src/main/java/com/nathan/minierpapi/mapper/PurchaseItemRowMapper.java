6b701cdfc8501574168fb2c7fa2feaa0248f78e6
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-12-02 15:27:30 +0300

    Feat(purchase): Purchase Item row mapper

---
+++ src/main/java/com/nathan/minierpapi/mapper/PurchaseItemRowMapper.java
@@ -0,0 +1,25 @@
+package com.nathan.minierpapi.mapper;
+
+import com.nathan.minierpapi.model.purchase.PurchaseItem;
+import com.nathan.minierpapi.utils.TimeUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class PurchaseItemRowMapper implements RowMapper<PurchaseItem> {
+
+    private final TimeUtils timeUtils;
+
+    public PurchaseItem mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new PurchaseItem(
+            rs.getString("product_id"),
+                rs.getInt("quantity"),
+                rs.getFloat("unit_cost"),
+                rs.getString("batch"),
+                timeUtils.convertToInstant(rs.getString("expiration_date"))
+        );
+    }
+}
