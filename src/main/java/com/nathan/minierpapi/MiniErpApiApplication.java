bcb89e5b8bd0f321699f67e702127a04c11442f5
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-12-04 10:26:56 +0300

    Fix: the application uses utc now to avoid gmt missPrecisions

--- src/main/java/com/nathan/minierpapi/MiniErpApiApplication.java
+++ src/main/java/com/nathan/minierpapi/MiniErpApiApplication.java
@@ -3,10 +3,13 @@ package com.nathan.minierpapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class MiniErpApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(MiniErpApiApplication.class, args);
     }
 
