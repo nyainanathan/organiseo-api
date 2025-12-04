cb2bf303fcc564201b8f399894187edcc991def2
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-12-04 09:16:16 +0300

    Feat(sale): new constructor without the items for the Sale class

--- src/main/java/com/nathan/minierpapi/model/Sale/Sale.java
+++ src/main/java/com/nathan/minierpapi/model/Sale/Sale.java
@@ -1,8 +1,11 @@
 package com.nathan.minierpapi.model.Sale;
 
+import lombok.AllArgsConstructor;
+
 import java.time.Instant;
 import java.util.List;
 
+@AllArgsConstructor
 public class Sale {
     private String id;
     private List<SaleItems> items;
@@ -10,4 +13,12 @@ public class Sale {
     private String paymentMethod;
     private Instant createdAt;
     private String cashierId;
+
+    public Sale(String id, double total, String paymentMethod, Instant createdAt, String cashierId) {
+        this.id = id;
+        this.total = total;
+        this.paymentMethod = paymentMethod;
+        this.createdAt = createdAt;
+        this.cashierId = cashierId;
+    }
 }
