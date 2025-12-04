7fee5ee478d83e9f2aeff3045639ec623c931661
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-12-04 18:14:57 +0300

    Feat(user): implementation of UserDetails in the User model

--- src/main/java/com/nathan/minierpapi/model/user/Users.java
+++ src/main/java/com/nathan/minierpapi/model/user/Users.java
@@ -3,16 +3,52 @@ package com.nathan.minierpapi.model.user;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class Users {
+public class Users implements UserDetails {
     private String id;
     private String username;
     private String fullName;
     private String role;
     private Instant  createdAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/nathan/minierpapi/utils/JwtUtil.java
@@ -0,0 +1,26 @@
+package com.nathan.minierpapi.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    private String generateToken(String username){
+
+    }
+}
