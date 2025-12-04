4394e8db32a6116a811e6f00ed9fa25f273f2a00
A. Ny Aina Nathan <nyaina.nathan.1@gmail.com>
2025-12-04 09:12:40 +0300

    Feat(sale): sales row mapper

---
+++ src/main/java/com/nathan/minierpapi/mapper/sale/SaleRowMapper.java
@@ -0,0 +1,27 @@
+package com.nathan.minierpapi.mapper.sale;
+
+import com.nathan.minierpapi.model.Sale.Sale;
+import com.nathan.minierpapi.model.Sale.SaleItems;
+import com.nathan.minierpapi.utils.TimeUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class SaleRowMapper implements RowMapper<Sale> {
+
+    private final TimeUtils timeUtils;
+
+    @Override
+    public Sale mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Sale(
+                rs.getString("id"),
+                rs.getDouble("total"),
+                rs.getString("payment_method"),
+                timeUtils.convertToInstant(rs.getString("created_at")),
+                rs.getString("cashier_id")
+        );
+    }
+}
